2858a33120b5abf16673e43059645b0d665a6086
eduardo <eduardo@debian>
2026-04-13 11:14:59 -0300

    feat: initial commit

---
+++ src/main/java/com/animelist/api_animes/ApiAnimesApplication.java
@@ -0,0 +1,13 @@
+package com.animelist.api_animes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAnimesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAnimesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/animelist/api_animes/ApiAnimesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.animelist.api_animes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAnimesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
